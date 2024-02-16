189bfc30806c6dc48dc012a80a6dd11a12f3c3a1
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-02-16 09:50:19 -0300

    create matrix  input

--- diagnostica_ex4.py
+++ diagnostica_ex4.py
@@ -8,6 +8,23 @@ def print_matrix_with_indices(matrix):
         # Print a new line after each row
         print()
 
+def input_matrix():
+    param = [[8,3,4], [1,5,9], [6,7,2]]
+    m = []
+    m1 = []
+    for i in range(len(param)):
+        m1.append(m,i)
+        m.clear()
+        for j in range(len(param[i])):
+            a=int(input(f"Insira o número da matriz na posição {i};{j}: "))
+            m.append(a)
+        print()
+        print(m)
+        print(m1)
+    return m1
+
 matriz = [[8,3,4], [1,5,9], [6,7,2]]
 
-print_matrix_with_indices(matriz)
+#print_matrix_with_indices(matriz)
+
+print(input_matrix())
